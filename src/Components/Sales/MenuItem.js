fe8de126916a6438a1e81f3c4e443caf58e11bd1
Bryan Diaz <bryan.diaz2016@gmail.com>
2022-12-22 22:40:55 -0600

    Menu items are now dynamic styled components to centralize all menu item imports such as images

--- src/Components/Sales/MenuItem.js
+++ src/Components/Sales/MenuItem.js
@@ -6,33 +6,52 @@ function MenuItem(props) {
   return (
     <>
       <ItemContainer>
-        <ItemDescription>This pupusa is so delicious.</ItemDescription>
-        <ImageContainer></ImageContainer>
+        <DescriptionContainer>
+          <ItemTitle>{props.title}</ItemTitle>
+          <ItemDescription>{props.description}</ItemDescription>
+        </DescriptionContainer>
+
+        <ImageContainer image={props.image}></ImageContainer>
       </ItemContainer>
     </>
   );
 }
 export default MenuItem;
 
+const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  //background-color: blue;
+  width: 70%;
+`;
+const ItemTitle = styled.div`
+  font-family: "Josefin Sans", sans-serif;
+  //  background-color: red;
+  font-size: 30px;
+`;
+
 const ItemDescription = styled.div`
   width: 100%;
-  height: fit-content:
+  height: fit-content;
+  font-size: 15px;
 `;
 
 const ImageContainer = styled.div`
   height: 150px;
   width: 150px;
-  background-color: blue;
-  background-image: url(${Pupusa});
+  //background-color: blue;
+  background-image: url(${(props) => props.image});
   background-position: center;
   background-repeat: no-repeat;
   background-size: 100%;
 `;
 
 const ItemContainer = styled.div`
-  background-color: blue;
+  background-color: #ddd7c4;
+  font-family: "Josefin Sans", sans-serif;
   display: flex;
-  height: 15vh;
+  justify-content: space-between;
+  height: fit-content;
   width: 50vh;
   border-radius: 8px;
   padding: 3%;
--- src/Components/Sales/PupusaMenu.js
+++ src/Components/Sales/PupusaMenu.js
@@ -2,9 +2,42 @@ import { React, useState } from "react";
 import MenuItem from "./MenuItem.js";
 import styled from "styled-components";
 import Pupusa1 from "./assets/pupusaItem2.webp";
+import Pupusa2 from "./assets/Loroco-Pupusa.jpg";
 
 function Menu(props) {
-  const menuItems = [{ title: "Frijol con Queso", image: Pupusa1 }];
+  const menuItems = [
+    {
+      title: "Revuelta",
+      image: Pupusa1,
+      description:
+        "Chicharron, queso y frijoles. Mixed (pork, cheese and beans)",
+    },
+    {
+      title: "Loroco con Queso",
+      image: Pupusa2,
+      description: "Pupusa con loroco y queso",
+    },
+    {
+      title: "Frijol con  Queso",
+      image: Pupusa1,
+      description: "Pupusa de frijol y queso",
+    },
+    {
+      title: "Chicharron con Queso",
+      image: Pupusa2,
+      description: "Pupusa con chicharron y queso",
+    },
+    {
+      title: "Jalapeño con queso",
+      image: Pupusa1,
+      description: "Pupusa con Jalapeño y queso",
+    },
+    {
+      title: "Espinaca con Queso",
+      image: Pupusa2,
+      description: "Pupusa con espinaca y queso",
+    },
+  ];
 
   return (
     <>
@@ -12,26 +45,32 @@ function Menu(props) {
         <MenuItem
           title={menuItems[0].title}
           image={menuItems[0].image}
+          description={menuItems[0].description}
         ></MenuItem>
         <MenuItem
-          title={menuItems[0].title}
-          image={menuItems[0].image}
+          title={menuItems[1].title}
+          image={menuItems[1].image}
+          description={menuItems[1].description}
         ></MenuItem>
         <MenuItem
-          title={menuItems[0].title}
-          image={menuItems[0].image}
+          title={menuItems[2].title}
+          image={menuItems[2].image}
+          description={menuItems[2].description}
         ></MenuItem>
         <MenuItem
-          title={menuItems[0].title}
-          image={menuItems[0].image}
+          title={menuItems[3].title}
+          image={menuItems[3].image}
+          description={menuItems[3].description}
         ></MenuItem>
         <MenuItem
-          title={menuItems[0].title}
-          image={menuItems[0].image}
+          title={menuItems[4].title}
+          image={menuItems[4].image}
+          description={menuItems[4].description}
         ></MenuItem>
         <MenuItem
-          title={menuItems[0].title}
-          image={menuItems[0].image}
+          title={menuItems[5].title}
+          image={menuItems[5].image}
+          description={menuItems[5].description}
         ></MenuItem>
       </MenuContainer>
     </>
